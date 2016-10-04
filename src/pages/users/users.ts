618e30e177bead32cf02b862d2a30095aa737d84
Chris Gaona <thebeloved88@sbcglobal.net>
2016-10-03 19:42:53 -0700

    Completes user search functionality

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -17,6 +17,7 @@ import {UserDetailsPage} from "../user-details/user-details";
 })
 export class UsersPage {
   users: User[];
+  originalUsers: User[];
 
   // in angular 2 we inject dependencies in the constructor as parameters
   constructor(
@@ -25,7 +26,12 @@ export class UsersPage {
   ) {
     githubUsers.load().subscribe(users => {
       this.users = users;
-    })
+      this.originalUsers = users;
+    });
+
+    // githubUsers.searchUsers('scotch').subscribe(users => {
+    //   console.log(users);
+    // });
   }
 
   // We then add a method that will handle the navigation, goToDetails.
@@ -34,5 +40,19 @@ export class UsersPage {
     // Ionic 2 treats navigation as a stack, meaning pages are added on top of each other.
     // That is why you see the this.navCtrl.push, and we push a page into the navigation stack.
     this.navCtrl.push(UserDetailsPage, {login});
+  }
+
+  search(searchEvent) {
+    let term = searchEvent.target.value;
+    // We will only perform the search if we have 3 or more characters
+    if (term.trim() === '' || term.trim().length < 3) {
+      // load cached users
+      this.users = this.originalUsers;
+    } else {
+      // Get the searched users from github
+      this.githubUsers.searchUsers(term).subscribe(users => {
+        this.users = users;
+      });
+    }
   }
 }
--- src/providers/github-users.ts
+++ src/providers/github-users.ts
@@ -35,4 +35,10 @@ export class GithubUsers {
       // This is returned as an observable, which we'll subscribe to to see the specific user
       .map(res => <User>res.json());
   }
+
+  // Search for github users
+  searchUsers(searchParam: string): Observable<User[]> {
+    return this.http.get(`${this.githubApiUrl}/search/users?q=${searchParam}`)
+      .map(res => <User[]>res.json().items);
+  }
 }
